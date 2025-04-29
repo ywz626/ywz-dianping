f1084c5fcabff877ff8d1c5fdaac0f1e1552a96a
sunwukong <1461507285@qq.com>
2025-04-29 21:31:56 +0800

    更使用redis保存登录信息的保存时间机制，增添了一个拦截所有的拦截器负责刷新保存时间

--- src/main/java/com/ywz/utils/LoginInterceptor.java
+++ src/main/java/com/ywz/utils/LoginInterceptor.java
@@ -19,40 +19,14 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-
-    private final StringRedisTemplate stringRedisTemplate;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //获取session中的用户信息
-//        HttpSession session = request.getSession();
-//        User user =(User) session.getAttribute("user");
-//        UserDTO userDTO = new UserDTO();
-//        BeanUtil.copyProperties(user,userDTO);
-        //获取redis中的用户信息
-        String authorization = request.getHeader("authorization");
-        if(authorization == null) {
-            log.error("用户token为空");
-            //如果没有登录，返回401状态码
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        UserDTO user = UserHolder.getUser();
+        if (user == null) {
+            response.setStatus(401);
             return false;
         }
-        Map<Object, Object> user = stringRedisTemplate.opsForHash().entries(RedisConstants.LOGIN_USER_KEY + authorization);
-        //判断用户是否登录
-        if(user.isEmpty() ){
-            log.error("该用户未登录");
-            //如果没有登录，返回401状态码
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
-        }
-        UserDTO userDTO = BeanUtil.fillBeanWithMap(user, new UserDTO(), false);
-        //如果存在，保存用户信息到ThreadLocal
-        UserHolder.saveUser(userDTO);
-
-        stringRedisTemplate.expire(RedisConstants.LOGIN_USER_KEY+authorization,RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
         return true;
     }
 }
