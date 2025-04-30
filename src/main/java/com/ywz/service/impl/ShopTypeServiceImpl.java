6cf53e5e50464be53d4855ebd354e06e5e9cc1d7
sunwukong <1461507285@qq.com>
2025-04-30 20:39:08 +0800

    增加对店铺类别的redis缓存用List结构实现

--- src/main/java/com/ywz/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/ywz/service/impl/ShopTypeServiceImpl.java
@@ -5,6 +5,7 @@ import com.ywz.entity.ShopType;
 import com.ywz.mapper.ShopTypeMapper;
 import com.ywz.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ywz.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -32,22 +33,47 @@ public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> i
     private Gson gson;
 
     @Override
-    public List<ShopType> queryByRedis() {
+    public List<ShopType> queryByRedisByString() {
         List<ShopType> typeList = new ArrayList<>();
-        for(int i = 1;i <= 10;i ++){
+        for (int i = 1; i <= SystemConstants.MAX_SHOPTYPE_SIZE; i++) {
             String s = stringRedisTemplate.opsForValue().get("shopType:" + i);
             ShopType shopType = gson.fromJson(s, ShopType.class);
-            if(shopType != null){
+            if (shopType != null) {
                 typeList.add(shopType);
             }
         }
-        if(typeList.isEmpty()){
+        if (typeList.isEmpty()) {
             // 如果没有数据，则从数据库中查询
             typeList = query().orderByAsc("sort").list();
             // 将数据存入Redis
-            for(ShopType shopType : typeList){
+            for (ShopType shopType : typeList) {
                 String s = gson.toJson(shopType);
                 stringRedisTemplate.opsForValue().set("shopType:" + shopType.getId(), s);
+            }
+            log.info("从数据库中查询商铺类型数据: {}", typeList);
+        } else {
+            log.info("从Redis中查询商铺类型数据: {}", typeList);
+        }
+        return typeList;
+    }
+
+    @Override
+    public List<ShopType> queryByRedisByList() {
+        List<String> typeListTemp = stringRedisTemplate.opsForList().range("shopType:", 0, -1);
+        List<ShopType> typeList = new ArrayList<>();
+        for (String s : typeListTemp) {
+            ShopType shopType = gson.fromJson(s, ShopType.class);
+            if (shopType != null) {
+                typeList.add(shopType);
+            }
+        }
+        if (typeList.isEmpty()) {
+            // 如果没有数据，则从数据库中查询
+            typeList = query().orderByAsc("sort").list();
+            // 将数据存入Redis
+            for (ShopType shopType : typeList) {
+                String s = gson.toJson(shopType);
+                stringRedisTemplate.opsForList().rightPush("shopType:", s);
             }
             log.info("从数据库中查询商铺类型数据: {}", typeList);
         }else {
