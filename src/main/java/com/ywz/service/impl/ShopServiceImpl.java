86e7285eee068eca171820da1548247f8e4e0aef
sunwukong <1461507285@qq.com>
2025-04-30 22:17:45 +0800

    学习了redis缓存穿透问题的缓存空对象策略并在查询商户不存在的情况应用

--- src/main/java/com/ywz/service/impl/ShopServiceImpl.java
+++ src/main/java/com/ywz/service/impl/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.ywz.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+import com.ywz.dto.Result;
 import com.ywz.entity.Shop;
 import com.ywz.mapper.ShopMapper;
 import com.ywz.service.IShopService;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -31,23 +33,28 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
-    public Shop getByRedisById(Long id) {
+    public Result getByRedisById(Long id) {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
         try {
+            Object o = stringRedisTemplate.opsForHash().get(key, "id");
+            log.info("o: {}", o);
+            if("null".equals(o)){
+                return Result.fail("redis缓存穿透商铺不存在");
+            }
             Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(RedisConstants.CACHE_SHOP_KEY + id);
-            log.info("从redis中获取商铺信息成功");
             Shop shop = BeanUtil.fillBeanWithMap(entries, new Shop(), false);
-            log.info("商户信息为：{}", shop);
-            return shop;
-        } catch (Exception e) {
-            log.error("{}从redis中获取商铺信息时出错", e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public void saveByRedis(Shop shop) {
-        try {
-            // 将商铺信息存入Redis
+            if (shop.getId() != null) {
+                log.info("redis中有商铺信息，直接返回");
+                return Result.ok(shop);
+            }
+            log.info("redis中没有商铺信息，查询数据库");
+            shop = getById(id);
+            if(shop == null) {
+                //如果数据库中没有商铺信息，则将null存入redis
+                stringRedisTemplate.opsForHash().put(key,"id","null");
+                stringRedisTemplate.expire(key, RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return Result.fail("商铺不存在");
+            }
             Map<String, Object> shopMap = BeanUtil.beanToMap(shop, new HashMap<>(), CopyOptions.create().
                     setIgnoreNullValue(true).
                     setFieldValueEditor((field, value) -> {
@@ -56,12 +63,27 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                         }
                         return value.toString();
                     }));
-            log.info("商户信息为：{}", shopMap);
             stringRedisTemplate.opsForHash().putAll(RedisConstants.CACHE_SHOP_KEY + shop.getId(), shopMap);
+            stringRedisTemplate.expire(RedisConstants.CACHE_SHOP_KEY+ shop.getId(), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
             log.info("商铺信息缓存到redis成功");
+            return Result.ok(shop);
         } catch (Exception e) {
-            log.error("{}缓存商铺信息到redis时出错", e.getMessage());
+            log.error("{}从redis中获取商铺信息时出错", e.getMessage());
+            throw new RuntimeException(e);
         }
+    }
+
+
+
+    @Override
+    public Result updateAndRedis(Shop shop) {
+        if(shop.getId() == null){
+            return Result.fail("商铺id不能为空");
+        }
+        updateById(shop);
+        //删除缓存
+        stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + shop.getId());
+        return Result.ok();
     }
 
 }
